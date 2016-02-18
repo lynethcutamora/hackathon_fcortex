66e0f3190cc4d33b1cab2226c87c3258d8d86ad3
lynethcutamora <lyneth.cutamora@gmail.com>
2016-02-19 01:30:19 +0800

    planning

--- application/views/pages/planning/content.php
+++ application/views/pages/planning/content.php
@@ -5,15 +5,27 @@
             <div class="col-md-7">
               <div class="box box-primary">
                 <div class="box-header with-border">
-                  <h4 class="box-title">Inputs</h4>
-                  <!-- <div class="box-tools pull-right">
-                    <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
-                    <button class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
-                  </div> -->
+                  <h4 class="box-title">
+                    <i class="fa fa-calendar"></i>&nbsp;&nbsp;<small><b>Inputs</b></small>
+                  </h4>
+                  <div class="box-tools pull-right">
+                    <!-- <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
+                    <button class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button> -->
+                    <span class="pull-right"><small>Date: &nbsp;&nbsp; <?php echo date('Y-m-d H:i:s');?></small></span>
+                  </div>
                 </div>
                 <div class="box-body">
-                  
+                  <form action="#" method="post">
+                    <div class="form-group">
+                      <div class="col-sm-5">
+                        <input type="text" class="form-control"name="inputsalary" id="inputsalary" placeholder="Input Monthly Salary"></textarea>
+                      </div>
+                    </div>
+                  </form>
                 </div><!-- /.box-body -->
+                <div class="box-footer">
+                  <input type="submit" class="btn btn-primary pull-right col-md-3" value="Save"></div>
+                </div>
               </div>
             </div>
           </div><!-- /.row -->
